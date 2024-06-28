199579a197944da582793c75183afff537ff82bf
mina <pichis1680@gmail.com>
2024-06-28 15:03:02 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './assets/header/Header'
+import './assets/common.css'
+import './assets/reset.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <main></main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/header/Header.jsx
@@ -0,0 +1,26 @@
+import './header.css'
+
+function Navi() {
+
+
+    return (
+      <header>
+        <nav>
+            <h1><a href="#">Mina Choi</a></h1>
+            <div className='ham'>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </nav>
+        <section>
+          <h2>PORT<br/>FOLIO</h2>
+          <h3>Mina Choi</h3>
+          <p>FRONT</p>
+        </section>
+      </header>
+    )
+  }
+  
+  export default Navi
+  
